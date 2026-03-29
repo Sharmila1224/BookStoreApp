caeda5643acb36baab37540c02d88acdb81cefc5
Sharmila1224 <sharmilashalini128@gmail.com>
2026-03-29 16:20:19 +0530

    Fix database issue for deployment

--- BookStoreApp/Program.cs
+++ BookStoreApp/Program.cs
@@ -1,8 +1,11 @@
 using BookStoreApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
